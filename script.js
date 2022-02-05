3790eb00ae5f5a9d8dfb71422a0578033ef2fa75
Yannis-Code <yannis.roche.12@gmail.com>
2022-02-05 18:49:34 +0100

    dividion des scripts

--- script.js
+++ script.js
@@ -15,35 +15,3 @@ function showResponsiveMenu() {
         root.style.overflowY = "";
     }
 }
-
-
-/* carroussel nul de pierre */
-
-// Makeshift carousel function that gets invoked with the Index to start it off, then the callback increments the index to recursively invoke the same function. Works even in IE11!
-var testimonialItems, time;
-var carouselDelay = 5000;
-document.addEventListener("DOMContentLoaded", function(event) {
-    testimonialItems = document.querySelectorAll(".radio_team");
-    time = setTimeout(() => autoCaroussel(0), carouselDelay);
-    pauseCaroussel();
-})
-
-function autoCaroussel(index) {
-    testimonialItems[index].checked = false;
-    index = (index + 1) % testimonialItems.length;
-    testimonialItems[index].checked = true;
-    time = setTimeout(() => autoCaroussel(index), carouselDelay);
-    pauseCaroussel();
-}
-
-function pauseCaroussel() {
-    document.getElementById("carousel_container").addEventListener("click", () => {
-        clearTimeout(time);
-        for (let i = 0; i < testimonialItems.length; i++) {
-            if (testimonialItems[i].checked == true) {
-                index = i;
-            }
-        }
-        time = setTimeout(() => autoCaroussel(index), carouselDelay);
-    })
-}
