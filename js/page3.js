af99667ffb7d249856616b608899b341f59e49d0
Yannis-Code <yannis.roche.12@gmail.com>
2022-02-07 23:18:37 +0100

    Commit final caroussel page3

--- js/page3.js
+++ js/page3.js
@@ -1,18 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    let container = document.querySelectorAll(".scrolling_caroussel");
-    let progressBar = document.querySelectorAll(".progressBar");
+    let hasTouch;
+    if ('ontouchstart' in document.documentElement) {
+        document.documentElement.classList.add('touch');
+        hasTouch = true;
+    } else {
+        document.documentElement.classList.add('no-touch');
+        hasTouch = false;
+    }
 
-    container.forEach((el, i) => {
-        el.addEventListener('wheel', function(e) {
-            if (e.deltaY != 0) {
-                e.preventDefault();
-                el.scrollLeft += e.deltaY;
-            }
-            let scroll = el.scrollLeft;
-            width = el.scrollWidth - el.offsetWidth,
-                scrolled = scroll / width * 100;
-            if (scrolled > 100) scrolled = 100;
-            progressBar[i].style.width = scrolled + "%";
+    let container = document.querySelectorAll(".scrolling_caroussel");
+
+    if (!hasTouch) {
+        container.forEach((el, i) => {
+            if (el.scrollWidth - el.offsetWidth <= 0) el.style.overflowX = "hidden";
+            el.addEventListener('wheel', function(e) {
+                if (e.deltaY != 0) {
+                    e.preventDefault();
+                    el.scrollLeft += e.deltaY;
+                }
+            })
         })
-    })
+    }
 })
