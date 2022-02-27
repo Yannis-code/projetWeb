64de7f6398168f9781110980a83bc2b51108e060
Yannis-Code <yannis.roche.12@gmail.com>
2022-02-27 14:18:50 +0100

    oui

--- js/script.js
+++ js/script.js
@@ -17,13 +17,15 @@ function showResponsiveMenu() {
 }
 
 function showAside() {
-    var aside = document.getElementById("myAside");
+    var aside = document.getElementById("toc_container");
     var btn = document.getElementById("hamburger_icon");
     if (aside.className === "") {
         aside.className = "open";
         btn.className = "open";
+        btn.innerHTML = "&#8249;"
     } else {
         aside.className = "";
         btn.className = "";
+        btn.innerHTML = "&#8250;"
     }
 }
