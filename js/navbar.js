67f9ccb6a6b61a1f044f084f8b299c1d5950f238
Yannis-Code <yannis.roche.12@gmail.com>
2022-03-02 08:00:58 +0100

    oui

--- js/navbar.js
+++ js/navbar.js
@@ -6,7 +6,6 @@ function showResponsiveMenu() {
     var menu = document.getElementById("topnav_responsive_menu");
     var icon = document.getElementById("topnav_hamburger_icon");
     var root = document.getElementById("root");
-    var blur = document.getElementById("blur");
     if (menu.className === "") {
         for (let index = 0; index < body.children.length; index++) {
             const el = body.children[index];
@@ -17,6 +16,7 @@ function showResponsiveMenu() {
         menu.className = "open";
         icon.className = "open";
         root.style.overflowY = "hidden";
+        body.style.backdropfilter = "blur(2px)";
     } else {
         for (let index = 0; index < body.children.length; index++) {
             const el = body.children[index];
@@ -27,5 +27,6 @@ function showResponsiveMenu() {
         menu.className = "";
         icon.className = "";
         root.style.overflowY = "";
+        body.style.backdropfilter = "none";
     }
 }
